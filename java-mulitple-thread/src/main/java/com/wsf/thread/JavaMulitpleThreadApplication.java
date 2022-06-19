a9acf875ec46d811947ad8594bd981d9f3c8fcbb
wsf <wsf@servyou.com.cn>
2022-06-19 16:29:44 +0800

    第一次提交

---
+++ java-mulitple-thread/src/main/java/com/wsf/thread/JavaMulitpleThreadApplication.java
@@ -0,0 +1,13 @@
+package com.wsf.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMulitpleThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMulitpleThreadApplication.class, args);
+    }
+
+}
