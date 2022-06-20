36ee2dd611bc873da2e1370ae09d4d0f79c1f325
wsf <wsf@servyou.com.cn>
2022-06-20 09:24:36 +0800

    第一次提交

---
+++ java-mulitple-thread/src/test/java/com/wsf/thread/JavaMulitpleThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsf.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMulitpleThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
